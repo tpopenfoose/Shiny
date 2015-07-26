26b8c89533013735d601a016205ad4a0a090869c
tpopenfoose <tpopenfoose@earthlink.net>
2015-07-25 22:42:54 -0400

    added help

--- test.R
+++ test.R
@@ -1,3 +1,10 @@
 library(shiny)
 
 runApp()
+
+
+library(shinyapps)
+
+
+shinyapps::setAccountInfo(name='tpopenfoose', token='E25FA4AFD25853F663D896FFA7C1E385', secret='NuWYyOkotTYMI3FP+DOcEjhy7BElYW5+CU+mB05k')
+deployApp()
--- ui.R
+++ ui.R
@@ -6,6 +6,13 @@ shinyUI(fluidPage(theme=shinytheme("cerulean"),
                   fluidRow(
                       column(6,
                              wellPanel(
+                                 helpText("Help for this App:  this app is to plot and summarize samples from a triangle distribution.  ",
+                                          "A triangle distribution has a min (a, the lower limit), a max (b, the upper limit)",
+                                          "and the midpoint (c, the mode of the distribution).  ",
+                                          "Below you can change the number of random samples drawn with the slider.  ",
+                                          "You can change the values of \"a\", \"b\" and \"c\".  \"c\" must be greater than or equal to \"a\" and \"c\" ",
+                                          "must also be less than or equal to \"b\"!  You can also change the bandwidth for the density plot."),
+                                 br(), hr(), br(),
                                  sliderInput("n", "Sample size:", 1, 10000, 5000),
                                  br(), hr(), br(),
                                  uiOutput("dist1"),
